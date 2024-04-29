357bb85ee136faa690a17f2e72c1190f3a0d6685
Thomas <thomas@dillingdata.se>
2024-04-29 07:14:30 +0200

    Added background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import background from '../img/checklist.jpg'
+import { Bakbak_One } from 'next/font/google';
+import background from '../img/checklist2.jpg';
 
 export default function Home() {
+    console.log(background);
+
+    background.width = 1920;
+    background.height = 1080;
+
     return(
-        <div style={{ backgroundImage: "url(${background})"}}>
+        <div style={{ backgroundImage: `url(${background.src})` }}>
             <h1 className="text-center text-4xl h-screen">Hello World</h1>
-            <p className="text-center h-screen">Testing</p>
         </div>
     )
 }
