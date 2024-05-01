7741d61c1fc13d8dfb7949e5b648ef22dc524c15
Thomas <thomas@dillingdata.se>
2024-05-01 17:05:03 +0200

    Added email field and signup button, testing different colors on main text

--- src/app/input.tsx
+++ src/app/input.tsx
@@ -1,20 +1,14 @@
 
-export default function inputField() {
-    return (
-        <div className="text-center">
-            <p>Test</p>
-        </div>
-    )
-}
 
-/*
 const inputField = () => {
     return (
-        <div>
-
+        <div className="text-center text-[#1E90FF]">
+            <form>
+                <input className="rounded-lg h-10 w-80 mr-5 text-center" type="email" />
+                <button className="bg-[#1E90FF] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg" type="submit">Sign Up</button>
+            </form>
         </div>
     )
 }
 
 export default inputField;
-*/
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,22 @@ export default function Home() {
         backgroundSize: 'cover',
         width: '100vw', // Set width to full viewport width
         height: '100vh', // Set height to full viewport height
+        display: 'flex',
+        alignItems: 'center', // Center vertically
+        justifyContent: 'center', // Center horizontally
     };
-
 
     return(
         <div style={backgroundStyle}>
-            <div className='flex items-center justify-center'>
-                <h1 className="text-4xl text-[#CED3D0]">Hello World</h1>
+            <div className='text-center'>
+                <h1 className="text-4xl text-[#1E90FF]">Coming Soon</h1>
+                <h1 className="text-4xl text-[#008080]">Coming Soon</h1>
+                <h1 className="text-4xl text-[#2F4F4F]">Coming Soon</h1>
+                <h1 className="text-4xl text-[#FFFFFF]">Coming Soon</h1>
+                <h1 className="text-4xl text-[#333333]">Coming Soon</h1>
+                <h1 className="text-4xl text-[#ADD8E6]">Coming Soon</h1>
+                <h1 className="text-4xl text-[#483C32]">Coming Soon</h1>
+                
                 <InputField />
             </div>
         </div>
