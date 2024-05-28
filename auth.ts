5d0e12c1ab562d20e5db1abca5809b28e0e66103
Thomas <thomas@dillingdata.se>
2024-05-28 21:05:22 +0200

    updating prisma file

--- auth.ts
+++ auth.ts
@@ -14,4 +14,4 @@ export const { auth, signIn, signOut } = NextAuth({
       },
     }),
   ],
-});
+}); 
--- src/lib/prisma.tsx
+++ src/lib/prisma.tsx
@@ -1,19 +1,3 @@
-/*
-import { PrismaClient } from "@prisma/client";
-
-const PrismaClientSingleton = () => {
-    return new PrismaClient();
-};
-
-declare global {
-    var prisma: undefined | ReturnType<typeof PrismaClientSingleton>;
-}
-
-export const prisma = globalThis.prisma ?? PrismaClientSingleton();
-
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
-*/
-
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
